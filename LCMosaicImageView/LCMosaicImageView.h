2e3076059393550e7d11c74fc4f4d0fe2cdd05b0
李韧 <lr_5146@163.com>
2015-10-26 18:13:52 +0800

    change file

--- LCMosaicImageView/LCMosaicImageView.h
+++ LCMosaicImageView/LCMosaicImageView.h
@@ -23,9 +23,32 @@ typedef NS_ENUM(NSInteger, LCMosaicLevel) {
 
 @optional
 
+/**
+ *  This method will be called before the very beginning of each paint event.
+ *
+ *  @param imageView current image view
+ */
 - (void)imageViewWillMosaicImage:(LCMosaicImageView *)imageView;
+
+/**
+ *  This method will be called after the very beginning of each paint event.
+ *
+ *  @param imageView current image view
+ */
 - (void)imageViewDidMosaicImage:(LCMosaicImageView *)imageView;
+
+/**
+ *  This method will be called before the very last of each paint event.
+ *
+ *  @param imageView current image view
+ */
 - (void)imageViewWillFinishMosaicImage:(LCMosaicImageView *)imageView;
+
+/**
+ *  This method will be called after the very last of each paint event.
+ *
+ *  @param imageView current image view
+ */
 - (void)imageViewDidFinishMosaicImage:(LCMosaicImageView *)imageView;
 
 @end
@@ -34,15 +57,51 @@ typedef NS_ENUM(NSInteger, LCMosaicLevel) {
 
 - (instancetype)initWithImage:(UIImage *)image;
 
+/**
+ *  Use this method to reset to the very initial status of image view.
+ */
 - (void)reset;
 
+/**
+ *  Instance method to get a whole mosaic image. The LCMosaicLevel will be set to LCMosaicLevelDefault.
+ *
+ *  @return An instance of UIImage
+ */
 - (UIImage *)mosaicImage;
+
+/**
+ *  Instance method to get a whole mosaic image.
+ *
+ *  @param level LCMosaicLevel
+ *
+ *  @return An instance of UIImage
+ */
 - (UIImage *)mosaicImageAtLevel:(LCMosaicLevel)level;
 
+/**
+ *  Class method to get a whole mosaic image. The LCMosaicLevel will be set to LCMosaicLevelDefault.
+ *
+ *  @return An instance of UIImage
+ */
 + (UIImage *)mosaicImage:(UIImage *)image;
+
+/**
+ *  Class method to get a whole mosaic image at certain mosaic level.
+ *
+ *  @param level LCMosaicLevel
+ *
+ *  @return An instance of UIImage
+ */
 + (UIImage *)mosaicImage:(UIImage *)image atLevel:(LCMosaicLevel)level;
 
+/**
+ *  Use this method to control whether the image view is ready for paint event.
+ */
 @property (nonatomic, assign, getter=isMosaicEnabled) BOOL mosaicEnabled;
+
+/**
+ *  You can refer to the project page https://github.com/LazyClutch/LCMosaicImageView to see different kind of mosaic effect. A more mosaic level means that the image will get more blurred.
+ */
 @property (nonatomic, assign) LCMosaicLevel mosaicLevel;
 
 @property (nonatomic, weak) id<LCMosaicImageViewDelegate> delegate;
